adbaf3534f27e6f88d33fa0dc052819e2463a1c5
ThisIsLamer <mangushev.maks13@gmail.com>
2021-06-08 20:44:03 +0300

    refactor

--- sublime.py
+++ sublime.py
@@ -1,10 +1,10 @@
-from src.NautilusExtension import NautilusExtension as nautilus
+from src.NautilusExtension import NautilusExtension
 from gi.repository import Nautilus, GObject, Gio
 
 
 class SublimeExtension(GObject.GObject, Nautilus.MenuProvider):
     def __init__(self):
-        self.nautilus = nautilus(Nautilus, Gio, "subl", "Sublime Text", "ru")
+        self.nautilus = NautilusExtension(Nautilus, Gio, "subl", "Sublime Text", "ru")
 
     def get_file_items(self, window, files):
         return self.nautilus.get_file_items(window, files, True)
